f1d70f72a8f4077a0362ef861f0baee07e5ca8b8
Akash Limbu <akashwanem@hotmail.com>
2022-03-17 14:19:17 +0000

    created role table

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,23 @@
 DROP DATABASE IF EXISTS employee_db;
+-- creates database called employee_db
 CREATE DATABASE employee_db;
 
 USE employee_db;
 
+-- creates table called department
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
+);
+
+-- creates table called role
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE 
 );
 
