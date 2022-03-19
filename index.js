b13c1e1c360be4b31c87b69de97c5a2f0531300a
Akash Limbu <akashwanem@hotmail.com>
2022-03-19 20:26:42 +0000

    added connection to database

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
+// connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'employees_db'
+});
+
+// connects to sql server and sql database
+db.connect((err){
+    if (err) throw err;
+    console.log('successfully connected to employees database');
+    promptQuestions();
+});
+
+const promptQuestions = () => {
+
+}
